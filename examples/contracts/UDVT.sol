a56b1905cc43c5cd59af5b7330e87ed28a42deb5
“vish” <vishwa.mehta30@gmail.com>
2023-05-20 20:36:09 +0530

    add examples

--- examples/contracts/UDVT.sol
+++ examples/contracts/UDVT.sol
@@ -1,31 +1,51 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.8;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
 
-// Represent a 18 decimal, 256 bit wide fixed point type using a user-defined value type.
-type UFixed256x18 is uint256;
+type UserId is uint256;
+type UFixed is uint256;
 
-/// A minimal library to do fixed point operations on UFixed256x18.
+/// A minimal library to do fixed point operations on UFixed.
 library FixedMath {
     uint constant multiplier = 10**18;
 
-    /// Adds two UFixed256x18 numbers. Reverts on overflow, relying on checked
-    /// arithmetic on uint256.
-    function add(UFixed256x18 a, UFixed256x18 b) internal pure returns (UFixed256x18) {
-        return UFixed256x18.wrap(UFixed256x18.unwrap(a) + UFixed256x18.unwrap(b));
+    /// Adds two UFixed numbers. Reverts on overflow, 
+    /// relying on checked arithmetic on uint256.
+    function add(UFixed a, UFixed b) internal pure returns (UFixed) {
+        return UFixed.wrap(UFixed.unwrap(a) + UFixed.unwrap(b));
     }
-    /// Multiplies UFixed256x18 and uint256. Reverts on overflow, relying on checked
-    /// arithmetic on uint256.
-    function mul(UFixed256x18 a, uint256 b) internal pure returns (UFixed256x18) {
-        return UFixed256x18.wrap(UFixed256x18.unwrap(a) * b);
+    /// Multiplies UFixed and uint256. Reverts on overflow,
+    /// relying on checked arithmetic on uint256.
+    function mul(UFixed a, uint256 b) internal pure returns (UFixed) {
+        return UFixed.wrap(UFixed.unwrap(a) * b);
     }
-    /// Take the floor of a UFixed256x18 number.
+    /// Take the floor of a UFixed number.
     /// @return the largest integer that does not exceed `a`.
-    function floor(UFixed256x18 a) internal pure returns (uint256) {
-        return UFixed256x18.unwrap(a) / multiplier;
+    function floor(UFixed a) internal pure returns (uint256) {
+        return UFixed.unwrap(a) / multiplier;
     }
-    /// Turns a uint256 into a UFixed256x18 of the same value.
+    /// Turns a uint256 into a UFixed of the same value.
     /// Reverts if the integer is too large.
-    function toUFixed256x18(uint256 a) internal pure returns (UFixed256x18) {
-        return UFixed256x18.wrap(a * multiplier);
+    function toUFixed(uint256 a) internal pure returns (UFixed) {
+        return UFixed.wrap(a * multiplier);
     }
+}
+
+contract Contract {
+    using FixedMath for UFixed;
+
+    mapping(UserId => User) public users;
+
+    struct User {
+        UserId id;
+        string name;
+    }
+
+    function getUser(UserId userId) public view returns(User memory) {
+        return users[userId];
+    }
+
+    function add(UFixed a, UFixed b) public pure returns(UFixed) {
+        return a.add(b);
+    }
+
 }
